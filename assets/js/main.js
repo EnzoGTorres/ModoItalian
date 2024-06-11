0d92de5a94c7be6666aa18edbdf241759cd4f38f
Enzo Torres <enzotorres462@gmail.com>
2024-06-11 15:42:06 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/*==================== SHOW MENU ====================*/
+     
+    // Validate that variables exist
+    
+            // We add the show-menu class to the div tag with the nav__menu class
+            
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+    // When we click on each nav__link, we remove the show-menu class
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+
+// We validate if the user previously chose a topic
+
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
+
+// Activate / deactivate the theme manually with the button
+
+    // Add or remove the dark / icon theme
+
+    // We save the theme and the current icon that the user chose
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
